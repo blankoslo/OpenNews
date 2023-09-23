094356a81e3e702d8816b1f639ac26e33ac97ad2
Terje Uglebakken <430725+teug91@users.noreply.github.com>
2023-09-23 10:57:22 +0200

    add marquee

---
+++ dystopia-news/src/ArticleMarquee.tsx
@@ -0,0 +1,112 @@
+const RightNow = () => (
+    <div className="mx-4 animate-bounce inline-block pt-2">Akkurat nå!</div>
+)
+
+export const ArticleMarquee = () => (
+    <div className="relative flex overflow-x-hidden bg-red-500 text-white  text-xl">
+        <div className="animate-marquee whitespace-nowrap">
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+        </div>
+        <div className="absolute top-0 animate-marquee2 whitespace-nowrap">
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+            <RightNow />
+        </div>
+    </div>
+)

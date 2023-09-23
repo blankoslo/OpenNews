015c7a989aae7416bc3189b84455f9813f29290c
mathiasnh <mathias.neslow96@gmail.com>
2023-09-23 11:12:54 +0200

    use NewsEpsilon container

--- dystopia-news/src/News.tsx
+++ dystopia-news/src/News.tsx
@@ -1,61 +1,61 @@
-import './News.css'
+import "./News.css";
 
-import SmallNews from './components/SmallNews'
-import AlphaArticle from './components/alpha/Article'
-import EpsilonArticle from './components/epsilon/Article'
-import { Mode } from './mode'
+import SmallNews from "./components/SmallNews";
+import AlphaArticle from "./components/alpha/Article";
+import NewsEpsilon from "./containers/epsilon/News";
+import { Mode } from "./mode";
 
 export default function News({ mode }: { mode: Mode }) {
   switch (mode) {
     case Mode.Alpha:
-      return <>
+      return (
+        <>
+          <AlphaArticle
+            heading="Se Obama spise Chipotle i Storgata!"
+            url="/"
+            imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+            hoursAgo={2}
+            source="CNN Indonesia"
+          />
+          <SmallNews>
+            <AlphaArticle
+              heading="Se Obama spise Chipotle i Storgata!"
+              url="/"
+              imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+              hoursAgo={2}
+              source="CNN Indonesia"
+              size="small"
+            />
+            <AlphaArticle
+              heading="Se Obama spise Chipotle i Storgata!"
+              url="/"
+              imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+              hoursAgo={2}
+              source="CNN Indonesia"
+              size="small"
+            />
+            <AlphaArticle
+              heading="Se Obama spise Chipotle i Storgata!"
+              url="/"
+              imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+              hoursAgo={2}
+              source="CNN Indonesia"
+              size="small"
+            />
+          </SmallNews>
+        </>
+      );
+    case Mode.Gamma:
+      return (
         <AlphaArticle
           heading="Se Obama spise Chipotle i Storgata!"
           url="/"
           imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
           hoursAgo={2}
-          source='CNN Indonesia'
+          source="CNN Indonesia"
         />
-        <SmallNews>
-          <AlphaArticle
-            heading="Se Obama spise Chipotle i Storgata!"
-            url="/"
-            imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-            hoursAgo={2}
-            source='CNN Indonesia'
-            size="small"
-          />
-          <AlphaArticle
-            heading="Se Obama spise Chipotle i Storgata!"
-            url="/"
-            imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-            hoursAgo={2}
-            source='CNN Indonesia'
-            size="small"
-          />
-          <AlphaArticle
-            heading="Se Obama spise Chipotle i Storgata!"
-            url="/"
-            imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-            hoursAgo={2}
-            source='CNN Indonesia'
-            size="small"
-          />
-        </SmallNews>
-      </>
-    case Mode.Gamma:
-      return <AlphaArticle
-        heading="Se Obama spise Chipotle i Storgata!"
-        url="/"
-        imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-        hoursAgo={2}
-        source='CNN Indonesia'
-      />
+      );
     case Mode.Epsilon:
-      return <EpsilonArticle
-        heading="Se Obama spise Chipotle i Storgata!"
-        url="/"
-        imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-      />
+      return <NewsEpsilon />;
   }
 }

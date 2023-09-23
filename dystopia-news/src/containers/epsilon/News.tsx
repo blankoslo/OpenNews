14fae399d67c652fcf4b52d0b3d392597a2ed7a5
mathiasnh <mathias.neslow96@gmail.com>
2023-09-23 11:46:56 +0200

    add real data to epsilon page

--- dystopia-news/src/containers/epsilon/News.tsx
+++ dystopia-news/src/containers/epsilon/News.tsx
@@ -1,40 +1,47 @@
 import Article from "../../components/epsilon/Article";
+import data from "../../dataset/epsilon/epsilon.json";
 
 const NewsEpsilon = () => {
   return (
-    <div className="flex flex-col space-y-6">
+    <div className="flex flex-col space-y-6 mb-16">
       <Article
-        heading="Se Obama spise Chipotle i Storgata!"
+        heading={data[1].title}
         url="/"
-        imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+        imageUrl={data[1].imageUrl}
         marquee
       />
       <div className="grid grid-cols-3 gap-6">
         <Article
-          heading="Se Obama spise Chipotle i Storgata!"
+          heading={data[2].title}
           url="/"
-          imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+          imageUrl={data[2].imageUrl}
           size="small"
           marquee
           color="white"
         />
         <Article
-          heading="Se Obama spise Chipotle i Storgata!"
+          heading={data[6].title}
           url="/"
-          imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+          imageUrl={data[6].imageUrl}
           size="small"
           marquee
           color="blue"
         />
         <Article
-          heading="Se Obama spise Chipotle i Storgata!"
+          heading={data[4].title}
           url="/"
-          imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+          imageUrl={data[4].imageUrl}
           size="small"
           marquee
           color="black"
         />
       </div>
+      <Article
+        heading={data[5].title}
+        url="/"
+        imageUrl={data[5].imageUrl}
+        marquee
+      />
     </div>
   );
 };

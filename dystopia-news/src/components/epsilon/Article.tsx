b41c27af11c78506f9624d8b9b54923c7f830be0
mathiasnh <mathias.neslow96@gmail.com>
2023-09-23 11:08:51 +0200

    add colors to epsilon article headers

--- dystopia-news/src/components/epsilon/Article.tsx
+++ dystopia-news/src/components/epsilon/Article.tsx
@@ -1,51 +1,67 @@
-import clsx from 'clsx'
-import { ArticleMarquee } from '../../ArticleMarquee'
+import clsx from "clsx";
+import { ArticleMarquee } from "../../ArticleMarquee";
 
 interface ArticleProps {
-    heading: string
-    url: string
-    imageUrl?: string
-    bannerText?: string
-    size?: 'small' | 'large'
-    marquee?: boolean
+  heading: string;
+  url: string;
+  imageUrl?: string;
+  bannerText?: string;
+  size?: "small" | "large";
+  marquee?: boolean;
+  color?: "red" | "blue" | "white" | "black";
 }
 
 const Article = ({
-    heading,
-    url,
-    imageUrl,
-    bannerText,
-    size = 'large',
-    marquee = false,
+  heading,
+  url,
+  imageUrl,
+  bannerText,
+  size = "large",
+  marquee = false,
+  color = "red",
 }: ArticleProps) => {
-    return (
-        <a href={url}>
-            {marquee && <ArticleMarquee />}
-            <div>
-                <img
-                    src={imageUrl}
-                    className={clsx('w-full object-cover object-top', {
-                        'h-40': size === 'small',
-                        'h-80': size === 'large',
-                    })}
-                />
-                {bannerText && (
-                    <p className="p-3 bg-yellow-200 uppercase text-black font-bold">
-                        {bannerText}
-                    </p>
-                )}
-                <p
-                    className={clsx('break-words p-4 font-bold text-white', {
-                        'bg-red-500': true,
-                        'text-5xl': size === 'large',
-                        'text-xl': size === 'small',
-                    })}
-                >
-                    {heading}
-                </p>
-            </div>
-        </a>
-    )
-}
+  return (
+    <a href={url}>
+      {marquee && <ArticleMarquee />}
+      <div>
+        <img
+          src={imageUrl}
+          className={clsx("w-full object-cover object-top", {
+            "h-40": size === "small",
+            "h-80": size === "large",
+          })}
+        />
+        {bannerText && (
+          <p className={"p-3 bg-yellow-200 uppercase font-bold text-black"}>
+            {bannerText}
+          </p>
+        )}
+        <p
+          className={clsx(
+            "break-words font-bold",
+            {
+              "text-white": color === "red" || color === "black",
+              "text-black": color === "blue" || color === "white",
+            },
+            {
+              "bg-red-500": color === "red",
+              "bg-blue-500": color === "blue",
+              "bg-white": color === "white",
+              "bg-black": color === "black",
+              "text-5xl": size === "large",
+              "text-xl": size === "small",
+            },
+            {
+              "p-4": size === "large",
+              "p-2": size === "small",
+            }
+          )}
+        >
+          {heading}
+        </p>
+      </div>
+    </a>
+  );
+};
 
-export default Article
+export default Article;
--- dystopia-news/src/containers/epsilon/News.tsx
+++ dystopia-news/src/containers/epsilon/News.tsx
@@ -1,39 +1,42 @@
-import Article from '../../components/epsilon/Article'
+import Article from "../../components/epsilon/Article";
 
 const NewsEpsilon = () => {
-    return (
-        <div className="flex flex-col space-y-6">
-            <Article
-                heading="Se Obama spise Chipotle i Storgata!"
-                url="/"
-                imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-                marquee
-            />
-            <div className="grid grid-cols-3 gap-6">
-                <Article
-                    heading="Se Obama spise Chipotle i Storgata!"
-                    url="/"
-                    imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-                    size="small"
-                    marquee
-                />
-                <Article
-                    heading="Se Obama spise Chipotle i Storgata!"
-                    url="/"
-                    imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-                    size="small"
-                    marquee
-                />
-                <Article
-                    heading="Se Obama spise Chipotle i Storgata!"
-                    url="/"
-                    imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
-                    size="small"
-                    marquee
-                />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col space-y-6">
+      <Article
+        heading="Se Obama spise Chipotle i Storgata!"
+        url="/"
+        imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+        marquee
+      />
+      <div className="grid grid-cols-3 gap-6">
+        <Article
+          heading="Se Obama spise Chipotle i Storgata!"
+          url="/"
+          imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+          size="small"
+          marquee
+          color="white"
+        />
+        <Article
+          heading="Se Obama spise Chipotle i Storgata!"
+          url="/"
+          imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+          size="small"
+          marquee
+          color="blue"
+        />
+        <Article
+          heading="Se Obama spise Chipotle i Storgata!"
+          url="/"
+          imageUrl="https://akns-images.eonline.com/eol_images/Entire_Site/2014524/rs_600x600-140624090240-600.barack-obama-chipotle.ls.62414.jpg"
+          size="small"
+          marquee
+          color="black"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default NewsEpsilon
+export default NewsEpsilon;
